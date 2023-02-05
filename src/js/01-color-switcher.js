1dfe19703e727b8d9906bca22cda0d6a5ba59729
TaniaMudryk <99959425+TaniaMudryk@users.noreply.github.com>
2023-02-05 12:44:08 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,22 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
 let timerId = null;
 
 startBtn.addEventListener('click', onStart);
 stopBtn.addEventListener('click', onStop);
+
+function getBgColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function onStart() {
-  timerId = setInterval(() => {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
-  startBtn.disabled = true;
+  timerId = setInterval(getBgColor, 1000);
+  startBtn.toggleAttribute('disabled');
 }
 
 function onStop() {
